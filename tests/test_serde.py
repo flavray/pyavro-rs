762fcc3afdfced8a7c2fec77f4a0a86f34c27870
Antonio Uccio Verardi <antonio.uccio.verardi@gmail.com>
2018-06-29 14:55:41 +0100

    Add first test

---
+++ tests/test_serde.py
@@ -0,0 +1,27 @@
+from pyavro_rs import decode_str
+from pyavro_rs import encode_str
+from pyavro_rs import Reader
+from pyavro_rs import Schema
+from pyavro_rs import Writer
+
+
+def test_serde():
+    schema_json = """
+    {
+        "type": "record",
+        "name": "test",
+        "fields": [
+            {"name": "a", "type": "long", "default": 42},
+            {"name": "b", "type": "string"}
+        ]
+    }
+    """
+    schema = Schema(schema_json)
+    writer = Writer(schema)
+    writer.append({'a': 27, 'b': 'foo'})
+    writer.append({'b': 'bar'})
+    writer.flush()
+    output = writer.into()
+    reader = Reader(output, schema)
+    records = [record for record in reader]
+    assert records == [{'a': 27, 'b': 'foo'}, {'a': 42, 'b': 'bar'}]
